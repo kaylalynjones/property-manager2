ad4faaa8b083808c25713e8fc466a0a384ccb0e9
Jeremy <banjeremy@gmail.com>
2014-07-26 11:03:56 -0500

    Room isBedroom

--- app/models/room.js
+++ app/models/room.js
@@ -4,6 +4,7 @@ var Room = function(name, length, width){
   this.name = name;
   this.length = parseInt(length);
   this.width = parseInt(width);
+  this.bedroom = null;
 };
 
 Room.prototype.area = function() {
@@ -12,6 +13,14 @@ Room.prototype.area = function() {
 
 Room.prototype.cost = function(){
   return this.area() * 5;
+};
+
+Room.prototype.isBedroom = function() {
+  if (this.bedroom === null) {
+    this.bedroom = this.name.indexOf('bed') > -1;
+  }
+
+  return this.bedroom;
 };
 
 module.exports = Room;
--- test/unit/room.js
+++ test/unit/room.js
@@ -15,21 +15,33 @@ describe('Room', function(){
       expect(room.name).to.equal('closet');
       expect(room.length).to.equal(24);
       expect(room.width).to.equal(35);
+      expect(room.bedroom).to.be.null;
     });
   });
 
   describe('#area', function() {
     it('should calculate the area of a room', function() {
       var r1 = new Room('living', 12, 12);
+     
       expect(r1.area()).to.equal(144);
     });
   });
 
   describe('#cost', function() {
     it('should calculate the cost of a room', function() {
-      var room = new Room('bedroom', 8, 10);
-
-      expect(room.cost()).to.equal(8 * 10 * 5);
+      var r2 = new Room('bedroom', 8, 10);
+     
+      expect(r2.cost()).to.equal(8 * 10 * 5);
+    });
+  });
+  
+  describe('#isBedroom', function(){
+    it('should determine if room is a bedroom', function(){
+      var r1 = new Room('living', 12, 12);
+      var r2 = new Room('bedroom', 8, 10);
+      
+      expect(r1.isBedroom()).to.be.false;
+      expect(r2.isBedroom()).to.be.true;
     });
   });
 });
